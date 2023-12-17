aa41953481b36aa45c23563f646f6aeb2bec919a
Son3a <sonba7b1@gmail.com>
2023-12-17 14:28:19 +0700

    ..

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:english_app/values/app_assets.dart';
+import 'package:english_app/values/app_colors.dart';
+import 'package:english_app/values/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,12 +14,41 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor.secondColor,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: AppColor.secondColor,
+        title: Container(
+          alignment: AlignmentDirectional.centerStart,
+          child: Text('English today',
+              style: AppStyle.h3.copyWith(color: AppColor.textColor)),
+        ),
+        leadingWidth: 40,
+        leading: InkWell(
+            onTap: () {},
+            child: Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(left: 10),
+              child: Image.asset(AppAssets.menu),
+            )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("refresh");
+        },
+        child: Image.asset(AppAssets.refresh),
+      ),
       body: Column(
         children: [
-          Row(
-            children: [
-              Image.asset(AppAssets.menu)
-            ],
+          Container(
+            margin: const EdgeInsets.only(left: 30, right: 30),
+            alignment: Alignment.center,
+            child: Text(
+              '\"It is amazing how complete is the delusion that beauty is goodness\"',
+              style: AppStyle.h5.copyWith(
+                color: AppColor.textColor,
+              ),
+            ),
           )
         ],
       ),
